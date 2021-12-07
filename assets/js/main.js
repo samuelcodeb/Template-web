755d9f2557de1b4b8d58f4d65cb850ded30fc433
samuelcodeb <samuel.code02@hotmail.com>
2021-12-07 18:26:10 -0300

    Project Update - Finalização das estruturas HTML, CSS e JS

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* ――――――――――――――――――――――――――――――――――― Menu responsivo ―――――――――――――――――――――――――――――――――――  */
+
+/* === Fazendo menu aparecer === */
+/* Validação if se a constante existir */
+
+
+/* === Fazendo menu esconder === */
+/* Validação if se a constante existir */
+
+
+/* ――――――――――――――――――――――――――――――――――― Removendo menu mobile ―――――――――――――――――――――――――――――――――――  */
